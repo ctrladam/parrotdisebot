a441a099c58316beb99de97596c62f381cf22a6c
sikudm <sikudmserver@gmail.com>
2018-04-14 19:06:06 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login('NDM0NzU5NDg0NzM3MzIzMDM4.DbPE4g.L_uqMNQgx_NrUbcuQx4ulMDIMaQ');
+
+var request = require('request');
+var mcCommand = '/mc'; // Command for triggering
+var mcIP = '84.3.203.150'; // Your MC server IP
+var mcPort = 4444; // Your MC server port
+
+client.on('message', message => {
+    if (message.content === mcCommand) {
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.reply('Sikertelen kapcsolódás...');
+            }
+            body = JSON.parse(body);
+            var status = '*A szerver jelenleg offline*';
+            if(body.online) {
+                status = '**Minecraft** szerver **online**  -  ';
+                if(body.players.now) {
+                    status += '**' + body.players.now + '** játékos van a szerveren';
+                } else {
+                    status += '*Senki sincs a szerveren!*';
+                }
+            }
+            message.reply(status);
+        });
+    }
+});
